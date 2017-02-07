d381a7a6ab1b29e047c7e8c14796196e3b3bddb4
deividas777 <deividas777@hotmail.com>
2017-02-07 13:45:47 +0000

    Setup for Heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
@@ -28,8 +30,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.listen(3001, ()=> {
-  console.log('Started on port 3001');
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+   Todo.findById(id).then((todo) => {
+     if(!todo){
+       return res.status(404).send();
+     }
+     res.send({todo});
+
+   }).catch((e) => {
+     res.status(400).send();
+   });
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
