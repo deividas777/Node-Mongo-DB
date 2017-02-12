919fe66da82f93fcbf009a1e144235b5a30cc666
deividas777 <deividas777@hotmail.com>
2017-02-12 18:51:31 +0000

    Add Post /users route and token methods

--- server/server.js
+++ server/server.js
@@ -57,53 +57,69 @@ app.get('/todos/:id', (req, res) => {
 
 //Delete method
 
-app.delete('/todos/:id', (req, res) => {
-  var id = req.params.id;
+  app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
+    if (!ObjectID.isValid(id)) {
       return res.status(404).send();
     }
 
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    });
   });
-});
 
 //Update method
 
-app.patch('/todos/:id', (req, res) => {
-  var id = req.params.id;
-  var body = _.pick(req.body, ['text','completed']);
+    app.patch('/todos/:id', (req, res) => {
+      var id = req.params.id;
+      var body = _.pick(req.body, ['text','completed']);
 
-  if(!ObjectID.isValid(id)){
-    return res.status(404).send();
-  }
+      if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+      }
 
-if(_.isBoolean(body.completed) && body.completed){
-  body.completedAt = new Date().getTime();
-} else {
-  body.completed = false;
-  body.completedAt = null;
-}
+    if(_.isBoolean(body.completed) && body.completed){
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
 
-Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-  if(!todo){
-    return res.status(404).send();
-  }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+      if(!todo){
+        return res.status(404).send();
+      }
 
- res.send({todo: todo});
+     res.send({todo: todo});
 
-}).catch((e) => {
-  res.status(400).send();
+    }).catch((e) => {
+      res.status(400).send();
+    });
 });
 
+//User Register
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email','password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
+
+//Server Settings
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
